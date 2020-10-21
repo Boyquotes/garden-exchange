7982a7fc2d8c843ee33272d4a0d272f5d78b4d6c
Boyquotes <webmaster@linux-live-cd.org>
2020-10-21 17:55:25 +0200

    Remove Remember me cos bug

--- migrations/Version20201019033041.php
+++ migrations/Version20201019033041.php
@@ -14,18 +14,22 @@ final class Version20201019033041 extends AbstractMigration
 {
     public function getDescription() : string
     {
+
         return '';
     }
 
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE `sessions` (`sess_id` VARBINARY(128) NOT NULL PRIMARY KEY,`sess_data` BLOB NOT NULL,`sess_lifetime` INTEGER UNSIGNED NOT NULL,`sess_time` INTEGER UNSIGNED NOT NULL) COLLATE utf8mb4_bin, ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('DROP TABLE sessions');
     }
 }
