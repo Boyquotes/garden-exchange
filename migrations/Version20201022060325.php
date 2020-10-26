d036a42143cdd5d863ee9b480fac432589e8103e
Boyquotes <webmaster@linux-live-cd.org>
2020-10-26 17:09:57 +0000

    Fix bug migration for image libelle

--- migrations/Version20201022060325.php
+++ migrations/Version20201022060325.php
@@ -22,7 +22,7 @@ final class Version20201022060325 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE garden_image ADD libelle VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE garden_image ADD libelle VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
